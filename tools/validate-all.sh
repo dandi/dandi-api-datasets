5f1f949740da901fd69f8ba3e0104da8bb1ee95c
DANDI Meta-user <dandi@mit.edu>
2021-02-18 09:55:35 -0500

    [GH-10] Cronjob script for validating all assets

---
+++ tools/validate-all.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+set -ex
+
+PYTHON=$HOME/miniconda3/bin/python
+DANDISETS=/mnt/backup/dandi/dandisets
+
+cd "$(dirname "$0")"/..
+git reset --hard HEAD
+git clean -dxf
+git pull
+
+mkdir -p .cronlib
+cd .cronlib
+if [ -e dandi-cli ]
+then ( cd dandi-cli; git pull )
+else git clone https://github.com/dandi/dandi-cli
+fi
+dandi_version="$(cd dandi-cli; git describe)"
+
+$PYTHON -m virtualenv --clear venv
+. venv/bin/activate
+pip install ./dandi-cli
+
+cd ..
+
+rmdir dandisets
+ln -s "$DANDISETS" dandisets
+python tools/populate_dandiset_asset_yamls.py dandisets/[0-9]*
+
+(
+    echo '# Dandisets with NWB Errors'
+    (
+        for d in [0-9]*
+        do if [ -e "$d"/NWB-VALIDATION.errors ]
+           then echo "$d"
+           fi
+        done
+    ) | nl -n ln -w 1 -s '. '
+
+    echo
+
+    echo '# Dandisets with Conversion Errors'
+    (
+        for d in [0-9]*
+        do
+            if [ -e "$d"/CONVERSION.errors ]
+            then echo "$d"
+            fi
+        done
+    ) | nl -n ln -w 1 -s '. '
+
+    echo
+
+    echo '# Dandisets with Validation Errors'
+    (
+        for d in [0-9]*
+        do
+            if [ -e "$d"/VALIDATION.errors ]
+            then echo "$d"
+            fi
+        done
+    ) | nl -n ln -w 1 -s '. '
+
+    echo
+
+    echo '# Dandisets with Assets and No Errors'
+    (
+        for d in [0-9]*
+        do
+            if [ ! -e "$d"/NWB-VALIDATION.errors ] && [ ! -e "$d"/CONVERSION.errors ] && [ ! -e "$d"/VALIDATION.errors ] && ls "$d"/sub-* 2>/dev/null
+            then echo "$d"
+            fi
+        done
+    ) | nl -n ln -w 1 -s '. '
+
+) > SUMMARY.md
+
+# In case the repo was updated while we were working:
+git pull --autostash --rebase
+
+git add [0-9]* SUMMARY.md
+if ! git diff --quiet --cached
+then git commit -F - <<EOT
+Ran populate_dandiset_asset_yamls.py
+
+dandi $dandi_version
+$(pip freeze | grep -Pi '^(pynwb|hdmf)==')
+EOT
+     git push
+else echo "No changes to commit"
+fi
+
+# vim:set et sts=4:
