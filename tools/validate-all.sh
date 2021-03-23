1d2d68bd669ff9937adafaf04e2f02ec0a757062
John T. Wodder II <git@varonathe.org>
2021-03-23 10:55:27 -0400

    Extend script to validate against dev packages

--- tools/validate-all.sh
+++ tools/validate-all.sh
@@ -4,30 +4,7 @@ set -ex
 PYTHON=$HOME/miniconda3/bin/python
 DANDISETS=/mnt/backup/dandi/dandisets
 
-cd "$(dirname "$0")"/..
-git reset --hard HEAD
-git clean -dxf
-git pull
-
-mkdir -p .cronlib
-cd .cronlib
-if [ -e dandi-cli ]
-then ( cd dandi-cli; git pull )
-else git clone https://github.com/dandi/dandi-cli
-fi
-dandi_version="$(cd dandi-cli; git describe)"
-
-$PYTHON -m virtualenv --clear venv
-. venv/bin/activate
-pip install ./dandi-cli
-
-cd ..
-
-rmdir dandisets
-ln -s "$DANDISETS" dandisets
-python tools/populate_dandiset_asset_yamls.py dandisets/[0-9]*
-
-(
+function summarize {
     echo '# Dandisets with NWB Errors'
     (
         for d in [0-9]*
@@ -72,8 +49,33 @@ python tools/populate_dandiset_asset_yamls.py dandisets/[0-9]*
             fi
         done
     ) | nl -n ln -w 1 -s '. '
+}
 
-) > SUMMARY.md
+cd "$(dirname "$0")"/..
+git reset --hard HEAD
+git clean -dxf
+git checkout master
+git pull
+
+mkdir -p .cronlib
+cd .cronlib
+if [ -e dandi-cli ]
+then ( cd dandi-cli; git pull )
+else git clone https://github.com/dandi/dandi-cli
+fi
+dandi_version="$(cd dandi-cli; git describe)"
+
+$PYTHON -m virtualenv --clear venv
+. venv/bin/activate
+pip install ./dandi-cli
+
+cd ..
+
+rmdir dandisets
+ln -s "$DANDISETS" dandisets
+
+python tools/populate_dandiset_asset_yamls.py dandisets/[0-9]*
+summarize > SUMMARY.md
 
 # In case the repo was updated while we were working:
 git pull --autostash --rebase
@@ -85,6 +87,27 @@ Ran populate_dandiset_asset_yamls.py
 
 dandi $dandi_version
 $(pip freeze | grep -Pi '^(pynwb|hdmf)==')
+EOT
+     git push
+else echo "No changes to commit"
+fi
+
+
+git checkout dev
+git merge -s ours --no-commit master
+git read-tree -m -u master
+pip install git+https://github.com/hdmf-dev/hdmf git+https://github.com/NeurodataWithoutBorders/pynwb
+
+python tools/populate_dandiset_asset_yamls.py dandisets/[0-9]*
+summarize > SUMMARY.md
+
+git add [0-9]* SUMMARY.md
+if ! git diff --quiet --cached
+then git commit -F - <<EOT
+Ran populate_dandiset_asset_yamls.py
+
+dandi $dandi_version
+$(pip freeze | grep -Pi '^(pynwb|hdmf)')
 EOT
      git push
 else echo "No changes to commit"
