c6beaca5f1340211c0720102bd6571e3a4483b2b
Yaroslav Halchenko <debian@onerussian.com>
2021-05-10 14:39:30 -0400

    RF: no need to migrate2newschema since all metadata in the new schema now

--- tools/populate_dandiset_yaml.py
+++ tools/populate_dandiset_yaml.py
@@ -2,10 +2,8 @@
 
 from pathlib import Path
 import sys
-from dandi.metadata import migrate2newschema
 from dandi.dandiset import Dandiset
 from dandi.consts import dandiset_metadata_file
-from dandi.utils import yaml_dump, yaml_load
 from dandi.models import DandisetMeta
 import traceback
 
@@ -31,14 +29,7 @@ if __name__ == "__main__":
         dandiset = Dandiset(dspath)
         dandiset_meta = dandiset.metadata
         try:
-            new_meta = migrate2newschema(dandiset_meta)
-            # we still would get empty containers such as
-            # identifier: {}
-            # TODO: this all should be in dandi-cli
-            new_meta_json = new_meta.json(exclude_unset=True, exclude_none=True)
-            newmeta_path.write_text(yaml_dump(yaml_load(new_meta_json, "base")))
-            print(f"Dumped {newmeta_path} ", end="")
-            DandisetMeta.validate(new_meta)
+            DandisetMeta.validate(dandiset_meta)
             print("VALID ")
         except Exception as exc:
             traceback.print_exc(file=err_path.open('w'))
